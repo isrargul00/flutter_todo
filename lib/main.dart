f3d04ddec627a74fb734ff5adbeebed2bf1ba0ac
isrargul <isrargul00@gmail.com>
2022-01-12 13:46:26 +0500

    lesson1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MaterialApp(
-  home:Text('hey israr !.'),
-  
-));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App '),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            'hello israr ',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.blue[600],
+              fontFamily: 'Merriweather',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    ));
